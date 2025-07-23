fa97b558afea2b61046036964a3aa7193e92e3c7
João Gabriel Lenovo <joaogabriel9633@gmail.com>
2025-07-23 09:40:06 -0300

    Change color

--- src/templates/home/sections/hero-section/hero-section.tsx
+++ src/templates/home/sections/hero-section/hero-section.tsx
@@ -4,7 +4,7 @@ export function HeroSection() {
   return (
     <div
       className={cn(
-        "relative min-h-[29.8rem] sm:min-h-[26rem] bg-red-300 mt-[70px]"
+        "relative min-h-[29.8rem] sm:min-h-[26rem] bg-[#01C9E0] mt-[70px]"
       )}
     >
       <div className="absolute bg-[url('/BANNER-MOBILE-NOVO.png')] sm:bg-[url('/BANNER-TOPO.jpg')] inset-0 bg-cover bg-left sm:bg-center bg-no-repeat" />
